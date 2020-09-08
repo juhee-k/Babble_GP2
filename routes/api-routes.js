3b4d6deab1fd5ac7f52b923cca4d8a360d6ad0e1
tonyfig453 <65638676+tonyfig453@users.noreply.github.com>
2020-09-08 12:24:58 -0700

    added first name, last name to signup sheet

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,6 @@
 // Requiring our models and chatroom as we've configured it
 const db = require("../models");
 const chatroom = require("../config/passport");
-
 module.exports = function(app) {
   // Using the chatroom.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
@@ -13,7 +12,6 @@ module.exports = function(app) {
       id: req.user.id
     });
   });
-
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
@@ -29,13 +27,11 @@ module.exports = function(app) {
         res.status(401).json(err);
       });
   });
-
   // Route for logging user out
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
   });
-
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", (req, res) => {
     if (!req.user) {
